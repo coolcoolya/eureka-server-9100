d59cec4968d8c038b71ffde876437e772d950390
wangya <731716125@qq.com>
2020-02-21 18:21:55 +0800

    搭建多个注册中心，高可用集群eureka

---
+++ src/test/java/com/example/eurekaserver9100/EurekaServer9100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserver9100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer9100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
